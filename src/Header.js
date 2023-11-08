f5e95b8d81ab03d3e1d30dcfe3a077a2a836de08
AHMAD Ella <ellaahmad533@gmail.com>
2023-11-08 10:42:45 +0100

    items

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,22 @@
 function Header() {
   const isUserLogged = true;
+  const isUserLogged = fals;
+  const itemsInCart = 2;
     return (
       <header>
         <h1>my commerce</h1>
         <nav>
           <ul>
+          {isUserLogged ? <li>Ella</li> :
+            <li>connexion</li>}
             <li>Home</li>
             <li>Products</li>
             <li>New</li>
-            {isUserLogged ? <li>Ella</li> :
-            <li>connexion</li>}
+            
+            {itemsInCart >0 ?
+            <li> cart:{itemsInCart} </li>:<li>ther is no items</li>
+
+            }
           </ul>
         </nav>
       </header>
