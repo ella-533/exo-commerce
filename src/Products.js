0cf1bd7e85cd20f91c0c9d9a4cb7f4d84cb907e6
AHMAD Ella <ellaahmad533@gmail.com>
2023-11-08 14:15:59 +0100

    Array

--- src/Products.js
+++ src/Products.js
@@ -1,18 +1,41 @@
 function Products() {
-    const productsFromOb = [
-        "Mocassins à glands", "Charentaises", "Talons aiguille"
+   
+    const productsFromDb = [
+      {
+        title: "Paillasson",
+        price: 20,
+        isPublished: true,
+      },
+      {
+        title: "Aspirateur",
+        price: 340,
+        isPublished: true,
+      },
+      {
+        title: "Lampe",
+        price: 50,
+        isPublished: true,
+      },
+      {
+        title: "Casque vélo",
+        price: 60,
+        isPublished: true,
+      },
     ];
+  
     return (
-        <main>
-            {productsFromOb.map((element)=>{
-                return (
-                    <article>
-                        <h2>{element}</h2>
-                    </article>
-                );
-            })}
+      <main>
+        {productsFromDb.map((product) => {
+          return (
+            <article>
+           
+              <h2>{product.title}</h2>
+              <p>{product.price} e</p>
+            </article>
+          );
+        })}
         </main>
-    );
+  );
 }
   
 export default Products;
